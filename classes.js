eb57ac1ce43008f8551f50688f239dca34ac2ef8
Mariano Zeppa <zeppamariano@gmail.com>
2020-11-25 14:22:45 +1300

    Add functions that works with DOM

--- classes.js
+++ classes.js
@@ -1,12 +1,14 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+   one()
+   two()
+   three()
+   makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -20,6 +22,28 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
-// CREATE FUNCTION three HERE
 
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  // Next, we apply a new CSS class to it:
+  two.classList.add('green')
+}
+
+// CREATE FUNCTION three HERE
+function three () {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
+
+  // Next, we apply a new CSS class to it:
+  three.classList.add('pink')
+}
 // CREATE FUNCTION makeVisible HERE
+
+function makeVisible () {
+  var visible = document.getElementsByClassName('invisible');
+
+  visible[0].classList.add('visible');
+
+}
